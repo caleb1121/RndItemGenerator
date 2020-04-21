e9cfd95a3ec77f9c407dc810ba9d17c3666c6823
CalebPhillips <calebphillips132@gmail.com>
2020-04-21 13:19:53 +0100

    Add project files.

---
+++ RndItemGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RndItemGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generated item: Fighting Wasp");
+            Console.Write("Rarity level: " );
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Rare");
+            Console.ResetColor();
+            Console.WriteLine("");
+
+            Console.WriteLine("Stat one: + 5 attack");
+            Console.WriteLine("Stat two: +2 speed");
+            Console.WriteLine("Special ability: Wasp explosion!");
+        }
+    }
+}
